b6c67d4fc05bfd125943e3b05ad2dea56395cfec
mary <mary4net@gmail.com>
2025-05-28 17:15:17 -0400

    restore the dir structure

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,28 +1,6 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   version = '*',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   cmd = 'Neotree',
---   keys = {
---     { '<C-e>', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
---   },
---   opts = {
---     filesystem = {
---       window = {
---         position = 'left',
---         width = 30,
---       },
---     },
---   },
--- }
-
 return {
   -- If you want neo-tree's file operations to work with LSP (updating imports, etc.), you can use a plugin like
   -- https://github.com/antosha417/nvim-lsp-file-operations:
